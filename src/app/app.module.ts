fec138d07ea1362cda25f5348a9455673c308d43
unknown <annhgch18718@fpt.edu.vn>
2022-10-18 13:25:15 +0700

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavBarComponent, FooterComponent, MainComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
