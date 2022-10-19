f31f7da2587c39f42c1d49d1a413c3b6c4de27b6
Tien <annhgch18718@fpt.edu.vn>
2022-10-19 11:29:56 +0700

    blinding19-10

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  name: string = 'Tien';
+  googleURL: string = 'https://www.google.com/';
+  colSpan = 2;
+  count = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onBtnClick() {
+    console.log('Clicked');
   }
-
+  increase(event) {
+    console.log(event);
+    this.count += 1;
+  }
+  reduce() {
+    this.count -= 1;
+  }
 }
